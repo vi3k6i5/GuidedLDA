5213fc1686e8308fc2c545687b331f59f7973af2
Vikash Singh <vikash.duliajan@gmail.com>
2021-10-05 22:09:15 +0530

    corrected path for dataset

--- guidedlda/datasets.py
+++ guidedlda/datasets.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals  # noqa
 
 import os
 
-import lda.utils
+import guidedlda.utils
 
 
 _test_dir = os.path.join(os.path.dirname(__file__), 'tests')
@@ -13,7 +13,7 @@ NYT = 'nyt'
 
 def load_data(dataset_name):
     dataset_ldac_fn = os.path.join(_test_dir, dataset_name + '.ldac')
-    return lda.utils.ldac2dtm(open(dataset_ldac_fn), offset=0)
+    return guidedlda.utils.ldac2dtm(open(dataset_ldac_fn), offset=0)
 
 
 def load_vocab(dataset_name):
